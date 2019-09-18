b131625a76681f140674486b95e2c70c7c93598d
Brandon Stephen Salter <bss233@iris.nau.edu>
2019-09-18 11:10:11 -0700

    Started work on scripts

---
+++ Setup/Scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo This script will install the necessary packages required for the UDOO. This script must be run as sudo. 
+echo You will be prompted to enter your admin password
+read -p "Press enter to continue..."
+sudo -i
